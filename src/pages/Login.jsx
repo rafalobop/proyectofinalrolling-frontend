a8637ebed36a42d604e51963deae8ad8e48bab33
RocioDAndrea <dandrearocio1@gmail.com>
2021-06-08 17:06:55 -0300

    Agregando estilos al login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,21 +7,25 @@ import LoginForm from "../components/LoginForm";
 const Login = () => {
   return (
     <Container fluid>
-      <Row className="bg-dark">
-         <Col  xl={6} lg={12} className="login-bg">
-            <div className="position-absolute titulo p-4">
-                <h3 className="font-weight-bold text-dark text-uppercase">CodeSchool</h3>
+  <Row className="bg-dark">
+  <Col  xl={7} lg={7} className="login-bg d-none d-lg-block">
+     <div className="position-absolute titulo p-4">
+     <h3 className="font-weight-bold text-dark text-uppercase">CodeSchool</h3>
                 <p className="lead text-dark">descripcion</p>
             </div>
-         </Col>
-         <Col  xl={6} lg={12} className="d-flex">
-              <div className="container align-self-center p-6">
-                    <h1 className="font-weight-bold mb-3">Iniciar Sesión</h1>
+            </Col>
+
+            <Col  xl={5} lg={5} className="bg-dark d-flex flex-column align-items-end min-vh-100">
+          <div className="align-self-center w-100 px-lg-5 py-lg-4 p-4 mt-auto mb-auto">
+                    <h1 className="font-weight-bold mb-4">Iniciar Sesión</h1>
+                    
                     <p className="text-muted mb-5">Ingresa la siguiente información.</p>                    
-                     <LoginForm />
+                     
+                    <LoginForm />
+
                 </div>
-         </Col>
-       </Row>
+                </Col>
+            </Row>
 </ Container>
 
     
