12cf136975a81ead5e260ba8e6ca6f7ec8fc1efb
RocioDAndrea <dandrearocio1@gmail.com>
2021-06-09 16:16:27 -0300

    Importando logo

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-import Login from "./pages/Login";
+
 
 ReactDOM.render(
 
-<Login />,
+<App />,
 
 document.getElementById('root'));
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import {Container, Row, Col} from "react-bootstrap"
 import "../css/LoginForm.css"
-
+import logo from "../images/logo.png";
+import logo2 from "../images/logo2.png";
 import LoginForm from "../components/LoginForm";
 
 const Login = () => {
@@ -9,23 +10,21 @@ const Login = () => {
     <Container fluid>
   <Row className="bg-dark">
   <Col  xl={7} lg={7} className="login-bg d-none d-lg-block">
-     <div className="position-absolute titulo p-4">
-     <h3 className="font-weight-bold text-dark text-uppercase">CodeSchool</h3>
-                <p className="lead text-dark">descripcion</p>
-            </div>
-            </Col>
-
-            <Col  xl={5} lg={5} className="bg-dark d-flex flex-column align-items-end min-vh-100">
-          <div className="align-self-center w-100 px-lg-5 py-lg-4 p-4 mt-auto mb-auto">
-                    <h1 className="font-weight-bold mb-4">Iniciar Sesión</h1>
-                    
-                    <p className=" mb-5">Ingresa la siguiente información.</p>                    
+     <div className="logo w-50 position-absolute p-2 ml-2">
+     <img src={logo} class="img-fluid" />
+      </div>
+      </Col>
+        <Col  xl={5} lg={5} className="bg-dark d-flex flex-column align-items-end min-vh-100">
+          <div className="logo-sesion px-lg-5 pt-lg-4 pb-lg-3 p-4 mt-auto mb-auto">
+              <img src={logo2} className="w-100 mx-auto d-none-lg d-block" alt="img-fluid"/>
+              <h1 className="font-weight-bold mb-4">Iniciar Sesión</h1>
+              <p className=" mb-5">Ingresa la siguiente información.</p>                    
                      
-                    <LoginForm />
+                <LoginForm />
 
-                </div>
-                </Col>
-            </Row>
+          </div>
+        </Col>
+  </Row>
 </ Container>
 
     
