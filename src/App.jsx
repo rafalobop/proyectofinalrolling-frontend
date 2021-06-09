814636b3759f7d2ead02eaa4da61754af65e88d9
RocioDAndrea <dandrearocio1@gmail.com>
2021-06-09 16:08:42 -0300

    Añadiendo ruta login a App

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
         
            <Switch>
 
-            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={Login} />
 
             </Switch>
          
