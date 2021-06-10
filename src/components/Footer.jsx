9cac2f41aa92cee9e9c35deb2c965d7238b92437
unknown <r.loboplaza@hotmail.com>
2021-06-10 17:57:23 -0300

    cards del equipo agregadas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,61 +39,56 @@ const Footer = () => {
                     <div className="card" key={datos.id}>
                       <div className="face back">
                         <div className="card-back-container">
-                          <div className="back-img-container">
-                            <img
-                              src={datos.imagen}
-                              alt={datos.name}
-                              className="back-img"
-                            />
+                          <div className="technologies">
+                            <div className="tech">
+                              <img src={html} alt="html" />
+                            </div>
+                            <div className="tech">
+                              <img src={css} alt="css" />
+                            </div>
+                            <div className="tech">
+                              <img src={js} alt="javascript" />
+                            </div>
+                            <div className="tech">
+                              <img src={git} alt="git" />
+                            </div>
+                            <div className="tech">
+                              <img src={bootstrap} alt="bootstrap" />
+                            </div>
+                            <div className="tech">
+                              <img src={react} alt="react" />
+                            </div>
+                            <div className="tech">
+                              <img src={node} alt="node" />
+                            </div>
+                            <div className="tech">
+                              <img src={mongo} alt="mongo" />
+                            </div>
                           </div>
                         </div>
                       </div>
                       <div className="face front">
                         <div className="card-front-container">
-                          <div className="card-item">
-                            <div className="card-header">
-                              <div className="card-title">
-                                <h3>{datos.name}</h3>
-                              </div>
+                          <div className="card-header">
+                            <div className="card-title">
+                              <h3>{datos.name}</h3>
                             </div>
-                            <div className="card-body">
-                              <div className="img-container">
-                                <img
-                                  src={datos.imagen}
-                                  alt={datos.name}
-                                  className="card-img"
-                                />
-                              </div>
+                          </div>
+                          <div className="card-body">
+                            <div className="img-container">
+                              <img
+                                src={datos.imagen}
+                                alt={datos.name}
+                                className="card-img"
+                              />
                             </div>
-                            <div className="card-footer">
-                              <div className="tech-info">
-                                <div className="technologies">
-                                  <div className="tech">
-                                    <img src={html} alt="html" />
-                                  </div>
-                                  <div className="tech">
-                                    <img src={css} alt="css" />
-                                  </div>
-                                  <div className="tech">
-                                    <img src={js} alt="javascript" />
-                                  </div>
-                                  <div className="tech">
-                                    <img src={git} alt="git" />
-                                  </div>
-                                  <div className="tech">
-                                    <img src={bootstrap} alt="bootstrap" />
-                                  </div>
-                                  <div className="tech">
-                                    <img src={react} alt="react" />
-                                  </div>
-                                  <div className="tech">
-                                    <img src={node} alt="node" />
-                                  </div>
-                                  <div className="tech">
-                                    <img src={mongo} alt="mongo" />
-                                  </div>
-                                </div>
-                              </div>
+                          </div>
+                          <div className="card-footer">
+                            <div className="tech-info">
+                              <p className="tech-info-title">
+                                Full Stack <br />
+                                Developer
+                              </p>
                             </div>
                           </div>
                         </div>
