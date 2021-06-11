7a32fef196b16d5cccb5f7ebce7e32293dd1e830
unknown <r.loboplaza@hotmail.com>
2021-06-11 11:28:57 -0300

    iniciando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import { SidebarData } from '../helpers/SidebarData';
 
 const Sidebar = () => {
   return (
     <>
       <div className="sidebar">
-        <ul className="sidebar-list">
-          {console.log(SidebarData)}
-          {/* {SidebarData.map((val, key) => {
-            console.log(val);
-            // return (
-            //   <li
-            //     key={key}
-            //     onClick={() => {
-            //       window.location.pathname = val.link;
-            //     }}
-            //   >
-            //     <div>{val.icon}</div>
-            //     <div>{val.title}</div>
-            //   </li>
-            // );
-          })} */}
-        </ul>
+        <ul className="sidebar-list"></ul>
       </div>
     </>
   );
