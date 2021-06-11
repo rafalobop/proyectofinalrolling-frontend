5c03261ef317cb50ac788c6fe5572660d00ae045
unknown <r.loboplaza@hotmail.com>
2021-06-11 18:01:12 -0300

    sidebar terminado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBook,
+  faCaretRight,
   faHome,
   faSignOutAlt,
   faUsers,
@@ -10,51 +11,65 @@ import {
 import '../css/sidebar.css';
 
 const Sidebar = () => {
+  const [toggle, setToggle] = useState(false);
+  const showSidebar = () => setToggle(!toggle);
+
   return (
     <>
       <div className="sidebar">
-        <ul className="sidebar-list">
-          <li className="list-item">
-            <div className="item-container">
-              <div className="item-icon">
-                <FontAwesomeIcon icon={faHome} />
+        <div className={toggle ? 'list-container show' : 'list-container'}>
+          <ul className="sidebar-list">
+            <li className="list-item">
+              <div className="item-container">
+                <div className="item-icon">
+                  <FontAwesomeIcon icon={faHome} />
+                </div>
+                <div className="item-link">
+                  <Link to="/home">Inicio</Link>
+                </div>
               </div>
-              <div className="item-link">
-                <Link to="/home">Inicio</Link>
+            </li>
+            <li className="list-item">
+              <div className="item-container">
+                <div className="item-icon">
+                  <FontAwesomeIcon icon={faUsers} />
+                </div>
+                <div className="item-link">
+                  <Link to="/alumnos">Alumnos</Link>
+                </div>
               </div>
-            </div>
-          </li>
-          <li className="list-item">
-            <div className="item-container">
-              <div className="item-icon">
-                <FontAwesomeIcon icon={faUsers} />
+            </li>
+            <li className="list-item">
+              <div className="item-container">
+                <div className="item-icon">
+                  <FontAwesomeIcon icon={faBook} />
+                </div>
+                <div className="item-link">
+                  <Link to="/materias">Materias</Link>
+                </div>
               </div>
-              <div className="item-link">
-                <Link to="/alumnos">Alumnos</Link>
+            </li>
+            <li className="list-item">
+              <div className="item-container">
+                <div className="item-icon">
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </div>
+                <div className="item-link">
+                  <Link to="/">Cerrar Sesión</Link>
+                </div>
               </div>
-            </div>
-          </li>
-          <li className="list-item">
-            <div className="item-container">
-              <div className="item-icon">
-                <FontAwesomeIcon icon={faBook} />
-              </div>
-              <div className="item-link">
-                <Link to="/materias">Materias</Link>
-              </div>
-            </div>
-          </li>
-          <li className="list-item">
-            <div className="item-container">
-              <div className="item-icon">
-                <FontAwesomeIcon icon={faSignOutAlt} />
-              </div>
-              <div className="item-link">
-                <Link to="/">Cerrar Sesión</Link>
-              </div>
-            </div>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="sidebar-toggle my-auto">
+          <div className="toggle-container">
+            <FontAwesomeIcon
+              icon={faCaretRight}
+              className="toggle-icon"
+              onClick={showSidebar}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
