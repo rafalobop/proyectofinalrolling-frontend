9539a284205848ae66ce4ae9670105c7a44f2ac7
unknown <r.loboplaza@hotmail.com>
2021-06-11 11:22:13 -0300

    armando sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import Sidebar from '../components/Sidebar';
 
 const Home = () => {
   return (
     <>
       <NavBar />
+      <Sidebar />
       <h1>Home</h1>
     </>
   );
