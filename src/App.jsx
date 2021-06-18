6f32617fe7a7ebc57e4623fc0baea71a7c912649
RocioDAndrea <dandrearocio1@gmail.com>
2021-06-18 18:08:57 -0300

    Falta agregar alumno nuevo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import TableAlumnos from './components/TableAlumnos';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
@@ -11,6 +14,7 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Login} />
           <Route path="/home" component={Home} />
+          <Route path="/alumnos" component={TableAlumnos} />
         </Switch>
       </Router>
     </>
