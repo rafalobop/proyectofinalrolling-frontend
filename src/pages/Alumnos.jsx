2064e3fe46a817b9b630f62c8047805c269b7824
RocioDAndrea <dandrearocio1@gmail.com>
2021-06-19 23:17:16 -0300

    Pagina Alumnos creada

---
+++ src/pages/Alumnos.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import TableAlumnos from "../components/TableAlumnos";
+import AddAlumno from "../components/AddAlumno";
+
+
+import '../css/tableAlumnos.css';
+
+const Alumnos = () => {
+
+  const [show, setShow] = useState(false);
+
+    return (
+      
+        <div className="container mt-5">
+      <div className="row">
+        <div className="col color-titulo">
+          <h1>Lista de Alumnos</h1>
+          <hr />
+          <>
+                <div className="col">
+                  <h3>Bienvenido</h3>
+                </div>
+                <AddAlumno setShow={setShow} show={show} />
+
+                {show === false && <TableAlumnos />}
+              </>
+          
+        </div>
+      </div>
+      </div>
+
+     
+    )
+}
+
+export default Alumnos
