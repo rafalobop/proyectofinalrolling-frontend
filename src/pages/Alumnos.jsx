0f32cd4cf0d75dd613fad337d1712d32a72ad0ad
unknown <r.loboplaza@hotmail.com>
2021-06-22 14:11:43 -0300

    update

--- src/pages/Alumnos.jsx
+++ src/pages/Alumnos.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import NavBar from '../components/NavBar';
+import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
 const Alumnos = () => {
   return (
     <>
+      <NavBar />
+      <Sidebar />
       <h1>Alumnos</h1>
+      <Footer />
     </>
   );
 };
--- src/pages/Materias.jsx
+++ src/pages/Materias.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import NavBar from '../components/NavBar';
+import Sidebar from '../components/Sidebar';
+import Footer from '../components/Footer';
 const Materias = () => {
   return (
     <>
+      <NavBar />
+      <Sidebar />
       <h1>Materias</h1>
+      <Footer />
     </>
   );
 };
